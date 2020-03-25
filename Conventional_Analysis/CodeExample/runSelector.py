fd45a052b0d30a860b2c7136ead0ef1e15dc5d6d
egramsta <egramsta@cern.ch>
2020-03-25 12:23:33 +0100

    Added example code for opendata analysis in ROOT

---
+++ Conventional_Analysis/CodeExample/runSelector.py
@@ -0,0 +1,52 @@
+from ROOT import *
+import sys, os, time
+
+t0 = time.time() 
+
+arg1 = sys.argv[1]  
+
+if arg1 == 'Data': 
+        input_dir = '../2lep/Data/'
+elif arg1 == 'MC': 
+        input_dir = '/home/eirikgr/Documents/openData_13TeV/MC/BSM_Signal_Samples/'
+        input_dir1 = '/home/eirikgr/Documents/openData_13TeV/MC/SM_Backgrounds/'
+
+
+myChain = TChain('mini') 
+
+
+for filename in os.listdir(input_dir):
+        if not '.root' in filename: continue 
+        print filename  
+        myChain.Add(input_dir+filename) 
+if arg1 == 'MC': 
+        for filename in os.listdir(input_dir1):
+                if not '.root' in filename: continue 
+                print filename  
+                myChain.Add(input_dir1+filename)
+
+if not os.path.exists('./Histograms'):
+    os.makedirs('./Histograms')
+if not os.path.exists('./Histograms/MC/'):
+    os.makedirs('./Histograms/MC')
+if not os.path.exists('./Histograms/Data/'):
+    os.makedirs('./Histograms/Data')
+
+entries = myChain.GetEntries() 
+
+print "-------------------------------------------"
+if arg1 == 'Data': 
+        print "Running on real data!"
+else: 
+        print "Running on Monte Carlo!" 
+print "Number of events to process: %d" %entries
+print "-------------------------------------------"
+
+if arg1 == 'Data': 
+        myChain.Process("MySelector.C", "Data")
+else: 
+        myChain.Process("MySelector.C", "MC") 
+
+t = int( time.time()-t0 )/60  
+
+print "Time spent: %d min" %t 
