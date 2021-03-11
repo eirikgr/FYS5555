cb4b57d57f538eb55f7ae15c2195a550327af897
Eirik Gramstad <egramsta@cern.ch>
2021-03-11 17:13:34 +0100

    Updated notebooks to 13 TeV analysis

--- Conventional_Analysis/Input/Make_Infofile.py
+++ Conventional_Analysis/Input/Make_Infofile.py
@@ -1,54 +1,155 @@
-from infofile import infos
-from tabulate import tabulate
+#from infofile import infos
+#from tabulate import tabulate
 import re
 
 backgrounds = []
 sample_name = []
 infile = open('Background_samples.txt')
 for line in infile: 
-    sample_name.append(line.split()[0])
-    name = line.split('.')[1]
-    backgrounds.append(name)
+    sample_name = line.split()[0]
+    #name = line.split('.')[1]
+    backgrounds.append(sample_name)
     #print word
-infile.close()   
-
-Diboson = [105985, 105986, 105987];
-Zjets = [147770, 147771, 147772];
-ttbar = [117049, 117050];
-singleTop =  [110090, 110091, 110119, 110140];
-Wjets = [167740, 167741, 167742, 167743, 167744, 167745, 167746, 167747, 167748];
-DY = [173041, 173042, 173043, 173044, 173045, 173046];
-Higgs = [160155, 160205, 161005, 161055]; 
+infile.close()
 
 
-all_info = []
-for name in backgrounds:
-    sample_info = []
-    info = infos[name]
-    keys = ['DSID', 'events', 'red_eff', 'sumw', 'xsec']
-    sample_info.append(name)
-    if info['DSID'] in Diboson: 
-        sample_info.append("Diboson")
-    if info['DSID'] in Zjets: 
-        sample_info.append("Zjets")
-    if info['DSID'] in ttbar: 
-        sample_info.append("ttbar")
-    if info['DSID'] in singleTop: 
-        sample_info.append("singleTop")
-    if info['DSID'] in Wjets: 
-        sample_info.append("Wjets")
-    if info['DSID'] in DY: 
-        sample_info.append("DY")
-    if info['DSID'] in Higgs: 
-        sample_info.append("Higgs")
-    for key in keys: 
-        sample_info.append(info[key])
-    all_info.append(sample_info)   
+Higgs = [341122,
+         341155,
+         341947,
+         341964,
+         344235,
+         345060,
+         345323,
+         345324,
+         345325,
+         345327,
+         345336,
+         345337,
+         345445]
+
+Wjets = [364156,
+         364157,
+         364158,
+         364159,
+         364160,
+         364161,
+         364162,
+         364163,
+         364164,
+         364165,
+         364166,
+         364167,
+         364168,
+         364169,
+         364170,
+         364171,
+         364172,
+         364173,
+         364174,
+         364175,
+         364176,
+         364177,
+         364178,
+         364179,
+         364180,
+         364181,
+         364182,
+         364183,
+         364184,
+         364185,
+         364186,
+         364187,
+         364188,
+         364189,
+         364190,
+         364191,
+         364192,
+         364193,
+         364194,
+         364195,
+         364196,
+         364197]
+
+Zjets = [364100,
+         364101,
+         364102,
+         364103,
+         364104,
+         364105,
+         364106,
+         364107,
+         364108,
+         364109,
+         364110,
+         364111,
+         364112,
+         364113,
+         364114,
+         364115,
+         364116,
+         364117,
+         364118,
+         364119,
+         364120,
+         364121,
+         364122,
+         364123,
+         364124,
+         364125,
+         364126,
+         364127,
+         364128,
+         364129,
+         364130,
+         364131,
+         364132,
+         364133,
+         364134,
+         364135,
+         364136,
+         364137,
+         364138,
+         364139,
+         364140,
+         364141]
 
 
-headers = ['Process', 'Class', 'Dataset ID', 'Events', 'Red eff', 'Sum of weights', 'Cross section']
+Diboson = [363356,
+           363358,
+           363359,
+           363360,
+           363489,
+           363490,
+           363491,
+           363492,
+           363493]
+
+ttbar = [410000]
+
+singleTop = [410011,
+             410012,
+             410013,
+             410014,
+             410025,
+             410026]
+
+
+topX = [410155,
+        410218,
+        410219]
+
+mc_categories = {"Diboson":Diboson,"Zjets":Zjets,"Wjets":Wjets,"singleTop":singleTop,"topX":topX,"Higgs":Higgs}
 outfile = open("Infofile.txt", "w")
-outfile.write(tabulate(all_info, tablefmt = "plain", numalign="left"))
+sample_info = {}
+outfile.write("### %s|%s|%s\n"%("NAME","CATEGORY","DSID"))
+for sample_name in backgrounds:
+    dsid = int(sample_name.split(".")[0].split("_")[-1])
+    name = sample_name.split('.')[1]
+    for mc in mc_categories.keys():
+        if dsid in mc_categories[mc]:
+            outfile.write("%s|%s|%i\n"%(name,mc,dsid))
+            break
+                          
 outfile.close()
 
 """
